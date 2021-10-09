0aff5be0a16252b1493c56c457434d8d2f3cd523
vmurali100 <vmurali100@gmail.com>
2021-10-09 16:03:35 +0530

    CRUD APP

---
+++ CRUD/create.js
@@ -0,0 +1,8 @@
+function createusers(){
+  var user={
+    fName:document.getElementById("fName").value,
+    lName:document.getElementById("lName").value,
+    email:document.getElementById("email").value
+  }
+  console.log(user)
+}
