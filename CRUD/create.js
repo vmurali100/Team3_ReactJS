266df0673187b0121a89d8a40437b954405a1cb3
vmurali100 <vmurali100@gmail.com>
2021-10-09 16:36:36 +0530

    Adding Code

--- CRUD/create.js
+++ CRUD/create.js
@@ -4,5 +4,17 @@ function createusers(){
     lName:document.getElementById("lName").value,
     email:document.getElementById("email").value
   }
-  console.log(user)
+
+  // XMLHttpRequest
+
+  var sendInfo = new XMLHttpRequest();
+  
+  sendInfo.onreadystatechange = function (){ // Will Excute On Every State
+    if(sendInfo.readyState == 4 && sendInfo.status ==201){
+        console.log("User Added Successfully")
+    }
+  }
+  sendInfo.open("POST","http://localhost:3000/users");
+  sendInfo.setRequestHeader("Content-Type","application/json");
+  sendInfo.send(JSON.stringify(user))
 }
