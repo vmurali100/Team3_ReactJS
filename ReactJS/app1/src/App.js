e2af3e529f60681c1dfee9cc5d353ca4962ef8f0
vmurali100 <vmurali100@gmail.com>
2021-10-28 21:16:54 +0530

    Adding

--- ReactJS/app1/src/App.js
+++ ReactJS/app1/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chandu, Harish } from "./Harish";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello From APP1</h2>
+      <Harish />
+      <Chandu />
     </div>
   );
 }
---
+++ ReactJS/app1/src/Harish.js
@@ -0,0 +1,8 @@
+export function Harish(){
+    return <h2>Hello From Harish Component</h2>
+}
+
+export function Chandu(){
+    return <h2>Hello From Chandu Component</h2>
+}
+
