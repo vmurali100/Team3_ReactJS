61a078bf3f86e41ae1e717ed4f291bac1388cf93
babu074 <babuchandra074>
2021-11-08 19:18:59 -0800

    a

--- ReactJS/app3/src/App.js
+++ ReactJS/app3/src/App.js
@@ -3,31 +3,11 @@ import './App.css';
 
 export default function App() {
   
-      let myStyles = {
-    background: 'pink',
-    width: '500px',
-    margin: '10px',
-    color: 'red',
-    height: '200px',
-  };
+     
   
   return (
     <div>
-      <div id="myDiv">
-        <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>
-      </div>
-
-      <div
-        id="content"
-        style={{ width: '400px', background: 'yellow', height: '200px' }}
-      >
-        <h2>Welome to Style in css</h2>
-      </div>
-
-      <div id="main" style={myStyles}>
-        <h3>Welcome to React Styles </h3>
-      </div>
+      
     </div>
   );
 }
