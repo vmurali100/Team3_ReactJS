737913043c35abb0cf4b2ada51b67855891d8d9f
vmurali100 <vmurali100@gmail.com>
2021-11-09 20:10:23 +0530

    Adding A Compoent Props

--- ReactJS/app3/src/Lokesh.jsx
+++ ReactJS/app3/src/Lokesh.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Lokesh(props) {
-    console.log(props.personInfo)
+export default function Lokesh(properties) {
+    console.log(properties.personInfo)
     return (
         <div>
             <h2>Hello From Lokesh Component`</h2>
             <p>Value In Child Compoent</p>
             <ul>
-                <li>Person First Name : {props.personInfo.fname}</li>
-                <li>Person Last Name : {props.personInfo.lname}</li>
-                <li>Person Email : {props.personInfo.email}</li>
+                <li>Person First Name : {properties.personInfo.fname}</li>
+                <li>Person Last Name : {properties.personInfo.lname}</li>
+                <li>Person Email : {properties.personInfo.email}</li>
             </ul>
         </div>
     )
