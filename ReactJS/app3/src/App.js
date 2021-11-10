bd4a081800022f704f9d05fc1e538fcbd7220c9e
vmurali100 <vmurali100@gmail.com>
2021-11-10 20:58:45 +0530

    Adding Use State

--- ReactJS/app3/src/App.js
+++ ReactJS/app3/src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Murali, Person, Student } from './Murali';
 import User from './User';
+import { useState } from 'react';
 
 function App() {
+  // React Hooks
+  // When Declare a vaiable , 
+  //we need to pass the value 
+  //and Function to change the calue
+  let [userMessage, setUserMessage] = useState("Welcome to App Compoent in React Hooks")
   return (
     <div className="App">
+      {/* <h2>Hello From Functional Component - App</h2>
+      <h2>{userMessage}</h2>
+      <button onClick={()=>{setUserMessage("Hello React Hook ... I am Changing you ...")}}>Change Message</button> */}
       <User/>
     </div>
   );
--- ReactJS/app3/src/User.jsx
+++ ReactJS/app3/src/User.jsx
@@ -5,7 +5,7 @@ export default class User extends Component {
     constructor() {
         super()
         this.state = {
-            greeings:"Welcome to Class Compoent", // This is string
+            greeings:"WELCOME FROM CLASS COMPONENT", // This is string
             person: {
                 fname: "Murali",
                 lname: "Krishna",
@@ -15,10 +15,12 @@ export default class User extends Component {
             users:["Murali","Krishna","Ram","Ravi"] // This is Array 
         }
     }
+
     render() {
         return (
             <div>
-                <h2>WELCOME FROM CLASS COMPONENT</h2>
+                <h2>{this.state.greeings}</h2>
+                <button onClick={()=>{this.setState({greeings:"Hello I am Changed By Set State ...SSS"})}}>Change Heading</button>
                 {/* <ul>
                     <li>Person First Name : {this.state.person.fname}</li>
                     <li>Person Last Name : {this.state.person.lname}</li>
