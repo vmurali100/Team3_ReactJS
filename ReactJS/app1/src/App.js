cec7c2847ec13cb2adf13415b9982baa9f982c5d
babu074 <babuchandra074>
2021-11-12 17:23:11 -1200

    add

--- ReactJS/app1/src/App.js
+++ ReactJS/app1/src/App.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ChanduClass, { HareeshClass } from "../../app1/src/ChanduClass";
 import AbcClass from "./AbcClass";
-import Bindhu from "./Bindhu";
+import{ Bindhu,  Employess,  Kavya,  Sindhu, Studentss } from "./Bindhu";
 import Chandu, { Babu, Syam } from "./Chandu";
 import { Harish, Lokesh } from "./Harish";
 import { Product1, Product2 } from "./Product";
@@ -13,10 +13,33 @@ import XyzClass from "./XyzClass";
 
 
 function App() {
-  let [component,setcomponent]=useState("false")
+  const[event,setevent] = useState(false)
+  const[show,setshow] =useState(false)
+  const[user,setuser]=useState(false)
+  const[emp,setemp]=useState(false)
+  const[student,setstudent]=useState(true)
   return (
     <div className="App">
-      {component && <Bindhu/>}
+      <button onClick={()=>{setevent("true")}}>Show</button>
+      {event && <Bindhu/>  }
+      <hr/>
+   <button onClick={()=>{setshow(!show)}}>
+     {show ?"Hide message":"show message"}
+   </button>
+      {show && <Sindhu/>}
+      <hr/>
+      <button onClick={()=>{setuser("true")}}>SHOW</button>
+      {user && <Kavya/>}
+      <hr/>
+      <button onClick={()=>{setemp(!emp)}}>
+        {emp ?"Hide emp":" show emp"}
+      </button>
+      {emp && <Employess/> }
+      <hr/>
+      <button onClick={()=>{setstudent(!student)}}>
+        {student ?"Hide details":"Show details"}</button>
+      {student && <Studentss/>}
+     
       {/* <h2>Hello From App1</h2>
       <Product1/>
       <Product2/>
--- ReactJS/app1/src/Bindhu.jsx
+++ ReactJS/app1/src/Bindhu.jsx
@@ -1,4 +1,52 @@
-import React from "react"
-export default function Bindhu() {
-    return <h2>Hello from bindhu component</h2>
+import React, { Component, useState } from "react"
+import { render } from "react-dom"
+export function Bindhu() {
+    return (
+        <div>
+            <h2>Hello from bindhu component</h2>
+        </div>
+    )
+}
+export const Sindhu = () => {
+    return (
+        <div>
+            <h2>Hello from sindhu component</h2>
+        </div>
+    )
+}
+export function Kavya() {
+    return (
+        <div>
+            <h2>Hello from Kavya component</h2>
+        </div>
+    )
+
+}
+export function Employess() {
+    const [emp, setemp] = useState([{ "fname": "Vinnu", "lname": "Chowdary", "email": "vinnu@gmail.com" },
+    { "fname": "Haritha", "lname": "Kumari", "email": "Haritha@gmail.com" },
+    { "fname": "Santhosh", "lname": "Kumar", "email": "Sk@gmail.com" }])
+    return (<div>
+        <ul>
+            {emp.map((emps, i) => {
+                return <li key={i}>{emps.fname} - {emps.lname} --{emps.email}</li>
+            })}
+        </ul>
+    </div>)
+
+
+}
+export const Studentss = () => {
+    const [student, setstudent] = useState([{ "name": "shyam", "city": "kurnool" },
+    { "name": "Bheem", "city": "kadapa" },
+    { "name": "kiran", "city": "Tirupati" }])
+    return (
+        <div>
+            <ul>
+                {student.map((students, i) => {
+                 return   <li key={i}>{students.name}  -{students.city}</li>
+                })}
+            </ul>
+        </div>
+    )
 }
