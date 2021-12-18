f6791d8ea96c146e40ff770f382a98f825705a9a
babu074 <babuchandra074>
2021-12-18 15:08:10 +0530

    add

---
+++ ReactJS/app9/src/LifecycleComA.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class LifecycleComA extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: '0'
+        }
+        console.log('hello from constructor')
+    }
+     ChangeMessage=()=>{
+         console.log("hello new component")
+      this.setState({count:this.state.count+1})
+    }
+static getDerviedStateFromProps(props,state){
+    console.log("Hello from getDerviedStateFromProps " ,props,state)
+    
+}
+
+
+
+
+    render() {
+        return (
+            <div>
+                <h2>render component</h2>
+<button type="button" onClick={this.ChangeMessage}>Click  </button>
+            </div>
+        )
+    }
+}
