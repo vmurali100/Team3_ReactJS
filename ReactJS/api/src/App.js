60e6cc3a984f492a008ee9330957f8a660c06c77
babu074 <babuchandra074>
2021-12-27 14:54:26 +0530

    add

---
+++ ReactJS/api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PNR from './RapidApi/PNR';
+
+function App() {
+  return (
+    <div className="App">
+      <PNR/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/api/src/RapidApi/PNR.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect,useState } from 'react'
+import axios from 'axios'
+
+export default function PNR() {
+    const [pnr, setpnr] = useState([])
+    
+    return (
+        <div>
+          <h1>hello</h1>  
+        </div>
+    )
+}
