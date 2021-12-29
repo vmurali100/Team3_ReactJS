8a26cf73d0d7abf701ee8c88842971cec196ff14
babu074 <babuchandra074>
2021-12-29 10:43:07 +0530

    adding

--- ReactJS/app8/src/App.js
+++ ReactJS/app8/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Forms from './Forms';
-import Sample1 from './Referances/Sample1';
-import Sample2 from './Sample2';
-import Samplecomponent from './Purecomponents/Samplecomponent';
-import Classcomp from './Purecomp/Classcomp';
+// import Forms from './Forms';
+// import Sample1 from './Referances/Sample1';
+// import Sample2 from './Sample2';
+// import Samplecomponent from './Purecomponents/Samplecomponent';
+// import Classcomp from './Purecomp/Classcomp';
+// import Clickhandle from './Clickhandle';
+// import Mousehover from './Mousehover';
+// import Click from './HOComponent/Click';
+// import Hover from './HOComponent/Hover';
+// import Clicker from './HOC1/Clicker';
+// import Maincomp from './RenderMethod/Maincomp';
 
 function App() {
   return (
@@ -13,7 +19,13 @@ function App() {
      {/* <Sample1/> */}
      {/* <Sample2/> */}
      {/* <Samplecomponent/> */}
-     <Classcomp/>
+     {/* <Classcomp/> */}
+     {/* <Clickhandle/> */}
+     {/* <Mousehover/> */}
+     {/* <Click/> */}
+     {/* <Hover/> */}
+     {/* <Clicker/> */}
+     {/* <Maincomp/> */}
     </div>
   );
 }
