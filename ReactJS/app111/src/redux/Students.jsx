076e9aa7808fd6a6be8a6e8f359e88896c953f4c
babu074 <babuchandra074>
2022-01-06 15:37:24 +0530

    adding changes

--- ReactJS/app111/src/redux/Students.jsx
+++ ReactJS/app111/src/redux/Students.jsx
@@ -1,10 +1,33 @@
 
-import React from 'react'
+import React,{Component} from 'react'
+import { connect } from 'react-redux'
+import { getAllAtudents } from './action'
 
-export default function Students() {
+ class Students extends Component {
+     
+    
+     ShowStudents=()=>{
+        console.log()
+     this.props.dispatch(getAllAtudents())
+    }
+    render(){
     return (
         <div>
             <h2>Click to get the students details</h2>
+            <button onClick={this.ShowStudents}>Click</button>
+            {this.props.students.map((stu,i)=>{
+                 return <h3 key={i}>{stu}</h3>
+            })}
         </div>
     )
 }
+ }
+function StateToPros(state){
+    console.log(state)
+    
+    return{
+        students:state.students
+
+    }
+}
+export default connect(StateToPros)(Students)
--- ReactJS/app111/src/redux/reducer/Hello.js
+++ ReactJS/app111/src/redux/reducer/Hello.js
@@ -1,16 +1,17 @@
 const defaultState={
-    students:["Chandu","Lokesh","Harish","Abi"]
+    students:[]
 }
+let allStudents=["Chandu","Lokesh","Harish","Abi"]
 const rootReducer=(state=defaultState,action)=>{
     console.log(state)
 
     switch (action.type) {
         case "GET_ALL_STUDENTS":
-          return state.students  
+          return allStudents;
             
     
         default:
-            return state;
+            return [   ]
     }
 }
 export default rootReducer
