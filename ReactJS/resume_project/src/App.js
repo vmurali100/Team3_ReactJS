831afe88cd51e766e92dab395b3a34d27a0e78c3
babu074 <babuchandra074>
2022-01-19 15:36:11 +0530

    boostrap adding

--- ReactJS/resume_project/src/App.js
+++ ReactJS/resume_project/src/App.js
@@ -10,27 +10,40 @@ import { Home } from "./Home";
 import { Users } from "./Users";
 function App() {
   return (
-  <div>
-    <h2>Hello from react router </h2>
-    <BrowserRouter>
-    <nav>
-        <Link to="/">Home</Link> ||{" "}
-        <Link to="about">About</Link>||  
-        <Link to="company">Company</Link>||
-        <Link to="users">Users</Link>
-      </nav>
-      <Routes>
+    <div>
+      {/* <h2>Hello from react router </h2> */}
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+    
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+
+                <a className="nav-link active"  href="/">Home</a>
+                <a className="nav-link" href="about">About</a>
+                <a className="nav-link" href="company">Company</a>
+                <a className="nav-link" href="users">Users</a>
+
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
 
 
-        <Route path="company" element={<Company />} />
-        <Route path="/" element={<Home />} />
-        <Route path="users" element={<Users />} />
-        <Route path="about" element={<About />} />
+          <Route path="company" element={<Company />} />
+          <Route path="/" element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="about" element={<About />} />
 
 
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
