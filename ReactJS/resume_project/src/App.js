cf29dfc6b6f702a1a6ad72ef9decf44fe941d56b
babu074 <babuchandra074>
2022-01-19 21:40:04 +0530

    project added

--- ReactJS/resume_project/src/App.js
+++ ReactJS/resume_project/src/App.js
@@ -4,40 +4,40 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { About } from "./About";
-import { Company } from "./Company";
-import { Home } from "./Home";
-import { Users } from "./Users";
+
+import './css/style.default.css'
+import './css/custom.css'
+// import { About } from "./About";
+// import { Company } from "./Company";
+// import { Home } from "./Home";
+// import { Users } from "./Users";
+import { Header } from "./Components/Header";
+import { Slider } from "./Components/Slider";
+import { About } from "./Components/About";
+import { Expertise } from "./Components/Expertise";
+import { Education } from "./Components/Education";
+import { Experience } from "./Components/Experience";
+import { Contact } from "./Components/Contact";
+import { Footer } from "./Components/Footer";
 function App() {
   return (
     <div>
+      
       {/* <h2>Hello from react router </h2> */}
       <BrowserRouter>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-
-                <a className="nav-link active" href="/">Home</a>
-                <a className="nav-link" href="about">About</a>
-                <a className="nav-link" href="company">Company</a>
-                <a className="nav-link" href="users">Users</a>
-
-              </div>
-            </div>
-          </div>
-        </nav>
-
-        <Routes>
-          <Route path="company" element={<Company />} />
-          <Route path="/" element={<Home />} />
-          <Route path="users" element={<Users />} />
+      <Header/>
+      <Slider/>
+      
+       <Routes>
+          <Route path="expertise" element={<Expertise />} />
+          <Route path="education" element={<Education />} />
+          <Route path="experience" element={<Experience />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+
+          
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
