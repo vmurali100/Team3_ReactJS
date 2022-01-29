ef31b21a9dc6cc1bcbbf91fdfc202db720dec3fe
abirami121297 <abirami121297@gmail.com>
2022-01-29 16:39:45 +0530

    add crud in election project

--- ReactJS/election/src/Components/DistrctsDetails.jsx
+++ ReactJS/election/src/Components/DistrctsDetails.jsx
@@ -1,12 +1,23 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 
 export const DistrctsDetails = () => {
+
+    const [dtsName, showdistName] = useState([])
+    const [isUpdate, setisUpdate] = useState(false);
     const [dtName, setdistrictName] = useState({
+        id:"",
         districtName: "",
         constituency: [],
     });
-    const [dtsName, showdistName] = useState([])
+    useEffect(() => {
+     axios.get("http://localhost:3000/districts").then((res)=>{
+         console.log(res.data);
+         showdistName(res.data)
+     })
+    }, []);
+    
+   
     const handleValues = (e) => {
         let newDistrictName = { ...dtName }
         newDistrictName[e.target.name] = e.target.value
@@ -17,16 +28,54 @@ export const DistrctsDetails = () => {
         let newDst = [...dtsName]
         newDst.push(dtName)
         console.log(newDst);
-        axios.post("http://localhost:3000/districts", dtName).then((res) => {
+       
+        axios.post("http://localhost:3000/districts/", dtName).then((res) => {
             console.log(res.data);
+            localStorage.setItem(("dtName"),JSON.stringify(dtName))
         })
         showdistName(newDst)
+        clearForm()
 
+    }
+    const editDistrictValues=(dtName)=>{
+        console.log(dtName);
+        setdistrictName(dtName)
+        setisUpdate(true)
+        console.log(isUpdate);
+    }
+    const deleteDetails=(dtName)=>{
+        console.log(dtName);
+        axios.delete("http://localhost:3000/districts/"+dtName.id).then((res)=>{
+        console.log("deleted");
+        })
+    }
+    const updateDistrictsDetails=()=>{
+       
+        axios.put("http://localhost:3000/districts/"+ dtName.id,dtName).then((res)=>{
+            console.log("updated");
+            
+        })
+        setisUpdate(false)
+        console.log(isUpdate);
+        clearForm()
+        
+    }
+    const clearForm=()=>{
+        let newDts={
+            id:"",
+            districtName: "",
+            constituency: [],
+        }
+        setdistrictName(newDts)
     }
     return <div className='container'>
         <div className="row">
             <div className="col-6">
                 <form>
+                <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Id</label>
+                        <input type="email" className="form-control" name='id' value={dtName.id}  onChange={(e) => { handleValues(e) }} aria-describedby="emailHelp" />
+                    </div>
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Enter Districts</label>
                         <input type="email" className="form-control" name='districtName' value={dtName.districtName} onChange={(e) => { handleValues(e) }} aria-describedby="emailHelp" />
@@ -35,9 +84,36 @@ export const DistrctsDetails = () => {
                         <label htmlFor="exampleInputEmail1" className="form-label">Enter Assembly Constituency</label>
                         <input type="email" className="form-control" name='constituency' value={dtName.constituency} onChange={(e) => { handleValues(e) }} aria-describedby="emailHelp" />
                     </div>
-
-                    <button type="button" onClick={addDistricts} className="btn btn-primary">Add Districts</button>
+                    {isUpdate == true &&  <button type="button" onClick={updateDistrictsDetails} className="btn btn-primary">Update Districts Details</button>}
+                   {isUpdate == false &&  <button type="button" onClick={addDistricts} className="btn btn-primary">Add Districts</button>}
+                   
+                    {/* <button type="button" onClick={deleteDetails} className="btn btn-primary">Delete Districts</button> */}
                 </form>
+            </div>
+            <br />
+            <div className="table">
+                <table border="1">
+                    <thead>
+                        <tr>
+                        <th>Id</th>    
+                        <th>DistrictName</th>
+                        <th>Constituency</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dtsName.map((val,i)=>{
+                            return <tr key={i}>
+                                <td >{val.id}</td>
+                                <td >{val.districtName}</td>
+                                <td >{val.constituency}</td>
+                                <td><button className='btn btn-warning' onClick={()=>{editDistrictValues(val)}}>Edit</button></td>
+                                <td><button className='btn btn-danger' onClick={()=>{deleteDetails(val)}}>Delete</button></td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
             </div>
         </div>
 
--- ReactJS/election/src/Components/Districts.jsx
+++ ReactJS/election/src/Components/Districts.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
@@ -7,30 +7,31 @@ export const Districts = () => {
     let navigate = useNavigate();
 
     const [places, setPlaces] = useState("");
-    const [searchPlaces, setsearchPlaces] = useState({});
+    // const [searchPlaces, setsearchPlaces] = useState({});
+    const [enteredValue, setEnteredValue] = useState([])
     const [loggedIn, setloggedIn] = useState(false);
     useEffect(() => {
         let users = JSON.parse(localStorage.getItem("loggedInUser"))
-        if(users){
+        if (users) {
             setloggedIn(true)
             navigate("/");
         }
     }, []);
-    
-    const submitPlace=()=>{
+
+    const submitPlace = () => {
         console.log(places);
-        if(loggedIn){
-            axios.get("http://localhost:3000/districts").then((res)=>{
+        if (loggedIn) {
+            axios.get("http://localhost:3000/districts").then((res) => {
                 console.log(res.data)
-                let findPlace=res.data.find(place => place.constituency.indexOf(places) >-1)
-                setsearchPlaces(findPlace)
-                console.log(findPlace);
+                let findPlace = res.data.find(place => place.constituency.indexOf(places) > -1)
+                setEnteredValue(findPlace)
+                // console.log(findPlace);
             })
-        }else{
+        } else {
             alert("Please Login to See the Results")
             navigate("/login");
         }
-        
+
     }
     return <div>
         {/* <h2>hello</h2> */}
@@ -40,14 +41,20 @@ export const Districts = () => {
                     <form>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Enter Assembly Constituency</label>
-                            <input type="email" className="form-control" value={places} onChange={(e)=>{setPlaces(e.target.value)}}  aria-describedby="emailHelp" />
+                            <input type="email" className="form-control" value={places} onChange={(e) => { setPlaces(e.target.value) }} aria-describedby="emailHelp" />
                         </div>
 
-                        <button type="button" onClick={submitPlace}  className="btn btn-primary">Find Districts</button>
+                        <button type="button" onClick={submitPlace} className="btn btn-primary">Find Districts</button>
+                      
+
                     </form>
                 </div>
-               {searchPlaces.districtName &&  <div className="col-6">
-                  <h4>Districts Name : {searchPlaces.districtName}</h4>
+                {console.log(enteredValue)}
+                {enteredValue.districtName && <div className="col-6">
+                    {/* <h4>Districts Name : {enteredValue.map((val) => {
+                        <li>{val.districtName}</li>
+                    })}</h4> */}
+                    <h4>Districts Name : {enteredValue.districtName}</h4>
                 </div>}
             </div>
         </div>
