cd289465bb2ea8c546a676a70fd35b927fa49d5b
babu074 <babuchandra074>
2022-01-31 15:24:58 +0530

    update changes

--- ReactJS/districts-project/src/components/DistrictDetails.jsx
+++ ReactJS/districts-project/src/components/DistrictDetails.jsx
@@ -24,10 +24,11 @@ export const DistrictDetails = () => {
         let dis = res.data.find((d) => d.constituency.indexOf(userInput) > -1)
         setsearchDist(dis)
       })
-    } else {
+    } else{
       alert("Please you can login first...")
       navigate("/login")
-    }
+      }
+    
   }
   return <div>
     <div className="container">
